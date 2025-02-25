03676e7cdf164e615f416c02e8a8380dc80f45ef
fthuggy <179480003+fthuggy@users.noreply.github.com>
2025-02-26 00:00:29 +0100

    Basic styling

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="portfolio-container">
+      <div className="first-box">
+        <h1>Fatou Taal</h1>
+        <p className="role">Frontend Developer</p>
+        <img
+                className="fatou-icon"
+                src="/images/Fatou.png"
+                alt="Animation portrait"
+              />
+      </div>
+      <div className="second-box">
+        <div className="vertical-btn">
+          <button className="btn" id="btn1">
+            About me
+          </button>
+          <button className="btn" id="btn2">
+            My work
+          </button>
+          <button className="btn" id="btn2">
+            Collaborations
+          </button>
+          <button className="btn" id="btn2">
+            CV
+          </button>
+          <button className="btn" id="btn3">
+            Contact
+          </button>
+        </div>
+      </div>
+      <footer>© 2025 Fatou Taal</footer>
     </div>
   );
 }
